c3ced8f871315c152759aa9a9957bdc331c049bd
JokuPelle <juuso.ylikoski@student.lut.fi>
2020-01-09 20:02:45 +0200

    first commit

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const bodyparser = require("body-parser");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const app = express();
+
+app.use(bodyparser.json());
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+    res.send("Home");
+});
+
+const db = require("./config/keys").mongoURI;
+mongoose.connect(db, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => console.log("Mongoose connected."))
+    .catch(err => console.log(err));
+
+const port = 8080;
+app.listen(port, () => console.log(`server started on port ${port}`));
+
